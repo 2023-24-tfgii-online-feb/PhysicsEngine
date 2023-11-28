16d547c5c0f4853b0faa43d1dbfdbd09dbebbfea
Daniel Meruelo Monzón <dmm1005@alu.ubu.es>
2023-11-28 22:32:57 +0100

    added React sketch component to the project.

--- WebApp/src/App.jsx
+++ WebApp/src/App.jsx
@@ -1,38 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Sketch from './Sketch';
+import './App.css';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR.
-        </p>
-        <h2>
-          Aquí es dónde estará el componente del canvas.
-        </h2>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Sketch />
+    </div>
+  );
 }
+
+
 
 export default App
