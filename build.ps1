0be75eb9d88c5243c19260aa6e8352c8179f7a26
Daniel Meruelo Monzón <dmm1005@alu.ubu.es>
2024-01-11 22:12:07 +0100

    Updated PS1 script

--- build.ps1
+++ build.ps1
@@ -1,14 +1,18 @@
 # PowerShell Script
 
+Write-Output "Building the React project..."
+
 # Navigate to WebApp directory
 Push-Location -Path .\WebApp
 
 # Install npm dependencies and build the project
-npm install
-npm run build
+npm install  | out-null
+npm run build  | out-null
 
 # Return to the original directory
 Pop-Location
+
+Write-Output "React project built successfully!"
 
 # Clear the contents of src\main\resources\static directory
 $staticPath = '.\src\main\resources\static\*'
@@ -19,8 +23,11 @@ $sourcePath = '.\WebApp\build\*'
 $destinationPath = '.\src\main\resources\static\'
 Copy-Item -Path $sourcePath -Destination $destinationPath -Recurse -Force
 
+Write-Output "React project copied to src\main\resources\static successfully!"
+Write-Output "Building the Spring Boot project..."
 # Build the project using Gradle
-.\gradlew.bat build
+.\gradlew.bat build  | out-null
+Write-Output "Spring Boot project built successfully!"
 
 # Run the Java application
 java -jar .\build\libs\TFG-1.0.0-RELEASE.jar
